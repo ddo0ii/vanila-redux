5ce159b616e468d4faa1ed316a8e0d8a95ce3918
sykwon <sykwon@nuvxx.com>
2025-09-18 17:54:45 +0900

    feat: 3.4 Deleting To Do

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import ToDo from "../routes/ToDo";
 
 const Home = ({ toDos, addToDo }) => {
   const [text, setText] = useState("")
@@ -21,7 +22,7 @@ const Home = ({ toDos, addToDo }) => {
       <button>Add</button>
     </form>
     <ul>
-      {JSON.stringify(toDos)}
+      {toDos.map(toDo => <ToDo {...toDo} key={toDo.id}/>)}
     </ul>
   </>
 }
